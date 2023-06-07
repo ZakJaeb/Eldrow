229abdc37f5aeb009bcfc2674628c85ef7978d94
ZakJaeb <zakjaeb@gmail.com>
2023-06-07 17:17:55 -0400

    Ask user six times for guess. Loop.

--- eldrow.py
+++ eldrow.py
@@ -1,7 +1,15 @@
 # eldrow.py
 
-guess = input("Guess a word: ")
+""" guess = input("Guess a word: ").upper()
 if guess == "SNAKE":
     print("Correct")
 else:
-    print("Wrong")
+    print("Wrong") """
+
+for guess_num in range(1, 7):
+    guess = input(f"\nGuess {guess_num}: ").upper()
+    if guess == "SNAKE":
+        print("Correct")
+        break
+
+    print("wrong")
