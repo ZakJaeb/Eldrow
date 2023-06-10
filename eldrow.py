06f542f90ca9669eaabc03180b3e38da976b7ac0
ZakJaeb <zakjaeb@gmail.com>
2023-06-10 17:24:05 -0400

    Tested playable.

--- eldrow.py
+++ eldrow.py
@@ -4,11 +4,31 @@ import pathlib
 import random
 from string import ascii_letters
 
+import pathlib
+import random
+from string import ascii_letters
+
+def main():
+    # Pre-process
+    word = get_random_word()
+
+    # Process (main loop)
+    for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
+
+        show_guess(guess, word)
+        if guess == word:
+            break
+
+    # Post-process
+    else:
+        game_over(word)
+
 def get_random_word():
-    WORDLIST = pathlib.Path("wordlist.txt").parent
+    wordlist = pathlib.Path(__file__).parent / "wordlist.txt"
     words = [
         word.upper()
-        for word in WORDLIST.read_text(encoding="utf-8").split("\n")
+        for word in wordlist.read_text(encoding="utf-8").split("\n")
         if len(word) == 5 and all(letter in ascii_letters for letter in word)
     ]
     return random.choice(words)
@@ -26,21 +46,8 @@ def show_guess(guess, word):
 
 def game_over(word):
     print(f"The word was {word}")
-    
-def main():
-    # Pre-process
-    word = get_random_word()
 
-    # Process (main loop)
-    for guess_num in range(1, 7):
-        guess = input(f"\nGuess {guess_num}: ").upper()
-
-        show_guess(guess, word)
-        if guess == word:
-            break
-
-    # Post-process
-    else
-        game_over(word)
+if __name__ == "__main__":
+    main()
 
 
